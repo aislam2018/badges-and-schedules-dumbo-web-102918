a0fddbca25c9aa0816add36b65e1d20882032b2c
anik1606 <anik1606@gmail.com>
2018-10-10 23:32:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,9 @@ def badge_marker(name)
   puts "Hello, my name is #{name}."
   return "Hello, my name is #{name}."
 end
+def batch_badge_creator(arr)
+  msg_arr = []
+  counter = 0 
+  while counter < arr.size 
+  
+  
